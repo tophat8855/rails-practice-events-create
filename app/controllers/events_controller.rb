6c063496a994fecc341a6b4e08cf23c9cd3df3b3
tophat8855 <hrfarley@gmail.com>
2014-12-03 11:40:29 -0800

    Delete

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -30,6 +30,12 @@ class EventsController < ApplicationController
     end
   end
 
+  def destroy
+    @event = Event.find(params[:id])
+    @event.destroy
+    redirect_to events_path
+  end
+
   private
   def event_params
     params.require(:event).permit(:date, :description, :requires_id, :capacity, :directions)
